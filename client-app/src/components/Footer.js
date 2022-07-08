6711f695a7030f1da339d0abba7515a69cbfe503
robertCodedIt <robertarmstrong999999@gmail.com>
2022-07-08 02:58:16 -0500

    commit: 02:57:59

---
+++ client-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <a href='https://www.github.com/robertcodedit/'>Robert Armstrong : Software Developer</a> &copy; 2022
+1      </footer>
+    )
+  }
+}
---
+++ client-app/src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Navigation extends Component {
+  render() {
+    return (
+     <nav>
+        <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>About</li>
+            <li>Resume</li>
+        </ul>
+     </nav>
+    )
+  }
+}
